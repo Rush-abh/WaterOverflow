b7587fea3613d57aba2191613d8f58bb9d03feba
Rush-abh <rushabh988@gmail.com>
2021-08-13 18:09:30 +1000

    Project structure and pseudo solution

---
+++ WaterOverflow/src/plexus/WaterOverflowMain.java
@@ -0,0 +1,17 @@
+package plexus;
+// Program to calculate liquid in jth glass in ith row with K liter water
+
+// To-do: Object oriented, validations, unit test, document, 2-3 hrs
+
+
+public class WaterOverflowMain {
+
+	// solution pseudo 
+	// take input of row, column and water(liter)
+	// find number of glasses (row+1)
+	// fill top glass
+	// subtract glass capacity, distribute(equally) remaining liquid in bottom two glass 
+	// got to next glass, subtract, distribute the remaining 
+	// display required glass quantity
+	
+}
