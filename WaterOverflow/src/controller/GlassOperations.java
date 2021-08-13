c28e21a778c4f33e74c8acc1ffc1ab7444f9269a
Rush-abh <rushabh988@gmail.com>
2021-08-13 20:18:19 +1000

    implementing operations, added in main app

--- WaterOverflow/src/controller/GlassOperations.java
+++ WaterOverflow/src/controller/GlassOperations.java
@@ -1,5 +1,59 @@
 package controller;
 
-public class GlassOperations {
+import java.util.Arrays;
 
+public class GlassOperations 
+{
+
+	float glass_size = 250; // volume in milliliters
+
+	// convert liter to ml
+	public float convertToMilliliters(float litre) {
+		return litre * 1000;
+	}
+
+	// return water in jth glass in ith row
+	public float findWater(int i, int j, float liter) 
+	{
+		
+		float X = convertToMilliliters(liter);
+
+		// find number of glasses
+		int g = Math.round((i * (i + 1) / 2 ));
+		float[] glass = new float[g + 5]; 
+
+		// fill top glass
+		int index = 0;
+		glass[index] = X;
+
+		// subtract glass capacity, distribute(equally) remaining liquid in bottom two
+		// glass
+		// got to next glass, subtract, distribute the remaining
+		for (int row = 1; row <= i; ++row) 
+		{
+			// Fill glasses in a given row. Number of columns in a row is equal to row
+			// number
+			for (int col = 0; col < row; ++col, ++index) 
+			{
+				System.out.println("r = " + row + " col = " + col + " index = " + index);
+				// Get the water from current glass
+				X = glass[index];
+
+				// Keep the amount less than or equal to capacity in current glass
+				glass[index] = (X >= glass_size) ? glass_size : X;
+
+				// Get the remaining amount
+				X = (X >= glass_size) ? (X - glass_size) : 0.0f;
+
+				// Distribute the remaining amount to the down two glasses
+				glass[index + row] += X / 2;
+				glass[index + row + 1] += X / 2;
+
+				System.out.println(Arrays.toString(glass));
+			}
+		}
+		
+		//Return specified glass index value
+				return glass[(int)(i * (i - 1) / 2 + j - 1)];
+	}
 }
--- WaterOverflow/src/plexus/WaterOverflowMain.java
+++ WaterOverflow/src/plexus/WaterOverflowMain.java
@@ -1,17 +1,34 @@
 package plexus;
 // Program to calculate liquid in jth glass in ith row with K liter water
-
 // To-do: Object oriented, validations, unit test, document, 2-3 hrs
 
+import java.util.Scanner;
+
+import controller.GlassOperations;
 
 public class WaterOverflowMain {
-
-	// solution pseudo 
-	// take input of row, column and water(liter)
-	// find number of glasses (row+1)
-	// fill top glass
-	// subtract glass capacity, distribute(equally) remaining liquid in bottom two glass 
-	// got to next glass, subtract, distribute the remaining 
-	// display required glass quantity
 	
+	public static void main(String[] args) {
+	
+	GlassOperations gops = new GlassOperations();
+	
+	// take input of row, column and water(liter)
+	Scanner my_scan = new Scanner(System.in);
+	
+	System.out.println("Enter the volume of water in liters: ");
+	float water_vol_float = my_scan.nextFloat();
+	
+	System.out.println("Enter the row number of the glass: ");
+	int row_val = my_scan.nextInt();
+	
+	System.out.println("Enter the column number of the glass: ");
+	int col_val = my_scan.nextInt();
+	
+	System.out.println("Water liters: "+water_vol_float+ " Glass Row: "+row_val+ "Glass column: "+col_val);
+	my_scan.close();
+	
+	// display required glass quantity
+	System.out.println("Amount of water in " + col_val + "th " + "glass of " + row_val + " row is: " +
+			gops.findWater(row_val, col_val, water_vol_float));
+	}
 }
